a0357eccdfa78cc4ece19a51f654115e04b9e627
M <SM169@ford.com>
2024-09-13 11:03:00 +0530

    adding 2 more files

---
+++ findfact.java
@@ -0,0 +1,11 @@
+public class FindFact{
+
+public static int findfactorial(int n){
+if(n==1){
+}
+}
+public static void main(String arg[]){
+int result=findfactorial(5);
+System.out.println("result:"+result);
+}
+}
